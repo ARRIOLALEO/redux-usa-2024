72e46f824efa734eb7fff12c4c0d5fa9531f79da
Mario Arriola <mario.arriola@commercetools.com>
2024-02-01 04:08:16 +0100

    feat: added the actions

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,14 @@
+import {useSelector,useDispatch} from 'react-redux';
+import {increment} from '../slices/counterSlice'
+
 const Counter = () =>{
+
+    const counter = useSelector((state)=> state.counter.value)
+    const dispatch = useDispatch()
+
     return (<div>
-        <h1>counter</h1>
-        <button onClick={()=>{}}>counter + 1</button>
+        <h1>counter {counter}</h1>
+        <button onClick={()=>dispatch((increment()))}>counter + 1</button>
     </div>)
 }
 
--- src/slices/counterSlice.js
+++ src/slices/counterSlice.js
@@ -1,6 +1,6 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-const initialState = {value:0}
+const initialState = {value:101}
 
 const counterSlice = createSlice({
     name: 'counter',
