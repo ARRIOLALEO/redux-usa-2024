f71a70e38dc65aba64fdd9a14e89526b24ed80cb
Mario Arriola <mario.arriola@commercetools.com>
2024-02-01 04:47:18 +0100

    chore: call api

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,19 @@
 import {useSelector,useDispatch} from 'react-redux';
-import {increment} from '../slices/counterSlice'
-
+import {increment,reset,callApi} from '../slices/counterSlice'
+import { useEffect } from 'react';
 const Counter = () =>{
-
+    
     const counter = useSelector((state)=> state.counter.value)
     const dispatch = useDispatch()
 
+    useEffect(()=>{
+        dispatch(callApi())
+    },[dispatch])
+    
     return (<div>
         <h1>counter {counter}</h1>
         <button onClick={()=>dispatch((increment()))}>counter + 1</button>
+        <button onClick={()=>dispatch((reset()))}>reset</button>
     </div>)
 }
 
--- src/slices/counterSlice.js
+++ src/slices/counterSlice.js
@@ -1,6 +1,12 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
 
-const initialState = {value:101}
+const initialState = {value:101,products:[],pending:true}
+
+export const callApi = createAsyncThunk('counter/callApi',async()=>{
+    const response = await fetch('https://fakestoreapi.com/products')
+    const data = await response.json()
+    return data
+})
 
 const counterSlice = createSlice({
     name: 'counter',
@@ -9,8 +15,17 @@ const counterSlice = createSlice({
         increment:(state) =>{state.value +=1},
         decrement:(state) =>{state.value -=1},
         multiply:(state) =>{state.value *=2},
+        reset:(state)=> initialState
+    },
+    extraReducers: (builder)=>{
+        builder.addCase(callApi.fulfilled,(state,actions)=>{
+            state.products = actions.payload
+        }).addCase(callApi.rejected,(state)=>{
+            state.pending = false
+            console.log("im calling the api but this time is kaput")
+        })
     }
 })
 
-export const {increment,decrement,multiply} = counterSlice.actions;
+export const {increment,decrement,multiply,reset} = counterSlice.actions;
 export default counterSlice.reducer;
